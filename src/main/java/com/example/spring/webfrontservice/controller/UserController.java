15704478b874f64d64488aa3b5e117b70e834b41
DongWoonKim <01030985843z@naver.com>
2024-10-31 12:11:12 +0900

    security

---
+++ src/main/java/com/example/spring/webfrontservice/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.spring.webfrontservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/webs")
+public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+}
