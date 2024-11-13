eaf62055aafd5af60b622f9905192df8915cae90
DongWoonKim <01030985843z@naver.com>
2024-11-13 15:10:47 +0900

    update code

--- src/main/java/com/example/spring/webfrontservice/config/SecurityConfig.java
+++ src/main/java/com/example/spring/webfrontservice/config/SecurityConfig.java
@@ -1,19 +1,11 @@
 package com.example.spring.webfrontservice.config;
 
-import com.example.spring.webfrontservice.config.security.HeaderAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -44,10 +36,6 @@ public class SecurityConfig {
                         .requestMatchers("/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
-
-                // Header 기반 인증 필터 추가
-//                .addFilterBefore(new HeaderAuthenticationFilter(),
-//                        UsernamePasswordAuthenticationFilter.class)
         ;
 
         return http.build();
