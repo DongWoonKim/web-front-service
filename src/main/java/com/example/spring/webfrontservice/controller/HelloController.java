7d28d7d93d182cbfae4eca9efe211c5c74e9ad6c
DongWoonKim <01030985843z@naver.com>
2024-12-09 13:46:06 +0900

    ~update

--- src/main/java/com/example/spring/webfrontservice/controller/HelloController.java
+++ src/main/java/com/example/spring/webfrontservice/controller/HelloController.java
@@ -27,15 +27,6 @@ public class HelloController {
     ) {
 
         System.out.println("authorizationHeader :: " + authorizationHeader);
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        // principal(subject/userId) 가져오기
-//        String userId = authentication.getName();  // 또는 authentication.getPrincipal().toString()
-
-        // roles/authorities 가져오기
-//        List<String> roles = authentication.getAuthorities().stream()
-//                .map(GrantedAuthority::getAuthority)
-//                .toList();
 
         return HelloResponseDTO.builder()
                 .message("It's ok.")
